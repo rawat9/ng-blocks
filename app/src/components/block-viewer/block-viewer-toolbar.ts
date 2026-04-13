d5a2577d1a98bf7631e007791243d715b79165e4
Anurag Rawat <anurag.rawat0201@gmail.com>
2026-04-14 00:21:19 +0100

    refactor: remove block viewer component and associated files

--- app/src/components/block-viewer/block-viewer-toolbar.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-block-viewer-toolbar',
-  imports: [],
-  template: ``,
-  providers: [],
-})
-export class BlockViewerToolbar {}
