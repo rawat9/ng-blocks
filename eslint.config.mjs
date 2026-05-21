b76a1d36dee57ad55792f22b57e4f275643b8c2c
Anurag Rawat <anurag.rawat0201@gmail.com>
2026-05-21 23:38:50 +0100

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc', '**/node_modules']
+    ignores: ['**/dist', '**/out-tsc', '**/node_modules', 'bun.lock']
   },
   {
     files: ['**/*.ts', '**/*.js'],
