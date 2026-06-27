d3c082d783959cff8cba5a574442a35564c641f3
Anurag Rawat <anurag.rawat0201@gmail.com>
2026-06-27 16:33:59 +0100

    update

--- app/src/app/pages/(blocks)/index.page.ts
+++ app/src/app/pages/(blocks)/index.page.ts
@@ -16,7 +16,6 @@ import {
   lucideZap
 } from '@ng-icons/lucide'
 import { blocks } from '#lib/registry'
-import { cn } from 'cnfast'
 import { BlocksLayout } from '../_components'
 
 @Component({
@@ -172,7 +171,6 @@ import { BlocksLayout } from '../_components'
 export default class Blocks {
   readonly blocks = blocks
   readonly isLoaded = signal(false)
-  readonly cn = cn
 
   constructor() {
     afterNextRender(() => {
